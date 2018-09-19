dcd8453022b3f059fdadd2e2489f922c8e1ee861
Paul Clark <clark.paul123@gmail.com>
2018-09-19 21:12:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget(idName, className) {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const ranksToIncrease = document.querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < ranksToIncrease.length; i++) {
+  ranksToIncrease[i].innerHTML = (parseInt((ranksToIncrease[i].innerHTML)) + n)
+  }
+  return ranksToIncrease;
+}
+
+function deepestChild() {
+  
+}
